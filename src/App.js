dd6392e8156b38a288a70a4d670a85fb45855b88
anshjain0106 <anshjain0106@gmail.com>
2023-10-03 00:32:33 +0530

    channel list under progress

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
+import {ChannelListContainer, ChannelContainer} from './components'
+import "./App.css"
 
 //to setup chat, we need api key
 const apiKey = "8ymsr2ghbgbh";
---
+++ src/components/TeamChannelPreview.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Avatar, useChatContext} from 'stream-chat-react'
+
+const TeamChannelPreview = ({channel, type}) => {
+    const {channel : activeChannel, client} = useChatContext();
+
+    const channelPreview = () => (
+        <p className='channel-preview__items'>#{channel?.data?.name || channel?.data?.id}</p>
+        //? indicating that channel should be there before accessing data
+    )//preview for channel with multiple users
+
+    //preview for DMs
+    const DirectPreview = () => {
+        const members = Object.values(channel.state.members).filter(({user}) => user.id !== client.userID)
+
+        return(
+            <div className="channel-preview__item single">
+                <Avatar 
+                image={members[0]?.user.image}//?indicate user exist
+                name={members[0]?.user.fullName}
+                size={24}
+                />
+                <p>{members[0]?.user?.fullName}</p>
+            </div>
+        )
+    }
+
+  return (
+    <div className={
+        channel?.id === activeChannel.id
+    }>
+      
+    </div>
+  )
+}
+
+export default TeamChannelPreview
